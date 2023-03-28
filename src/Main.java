9f0df1e0d85209d65a6887db78d0cfd621340661
SUHA <suha@zealand.dk>
2023-03-28 09:52:03 +0200

    Commit master1_1

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world! 2");
         System.out.println("Hello world! 2");
         System.out.println("Hello world! 2 Ny  udgave af master");
+        System.out.println("Hello world! 2 Ny  master1_1");
     }
 }
